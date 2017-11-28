8bc9e44a755480d9bdf9b3bf7c49ec3606eefe83
47X <o@kilku.com>
2017-11-28 01:51:30 +0100

    arduino: code to read input states

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -5,8 +5,8 @@
 #define audioLeft A0 //brown, 10k pulldown to A1 set as gnd
 #define audioRight A2 //white or any marked special
 #define audioGnd A1 //red, 10k pulldown to A1 set as gnd
-#define xlrPlay 2 //black
-#define xlrNext 3 //gray
+#define xlrPlayIn 2 //black
+#define xlrNextIn 3 //gray
 #define xlrGnd 4 //white
 #define nextBtnIn 5 //yellow
 #define nextBtnGnd 6 //orange
@@ -33,8 +33,8 @@ void setup() {
     pinMode(audioLeft, INPUT);
     pinMode(audioRight, INPUT);
     pinMode(audioGnd, OUTPUT);
-    pinMode(xlrPlay, INPUT_PULLUP);
-    pinMode(xlrNext, INPUT_PULLUP);
+    pinMode(xlrPlayIn, INPUT_PULLUP);
+    pinMode(xlrNextIn, INPUT_PULLUP);
     pinMode(xlrGnd, OUTPUT);
     pinMode(nextBtnIn, INPUT_PULLUP);
     pinMode(nextBtnGnd, OUTPUT);
@@ -49,10 +49,32 @@ void setup() {
     digitalWrite(playBtnGnd, 0);
 }
 
+//globals for reading state
+bool audioPlay;
+bool audioNext;
+bool buttonPlay;
+bool buttonNext;
+bool xlrPlay;
+bool xlrNext;
+
 void loop() {
   //read audio
+  audioPlay = analogRead(audioLeft);
+  audioNext = analogRead(audioRight);
   //read buttons and xlr
+  buttonPlay =! digitalRead(playBtnIn);
+  buttonNext =! digitalRead(nextBtnIn);
+  xlrPlay =! digitalRead(xlrPlayIn);
+  xlrNext =! digitalRead(xlrNextIn);
   //if audio > audioSensitivity or btns or pins execute action and wait
+  if (audioPlay > 10 or buttonPlay or xlrPlay) {
+    playAction();
+    delay(500);
+  }
+  if (audioNext > 10 or buttonNext or xlrNext) {
+    nextAction();
+    delay(500);
+  }
   //wait 1ms in loop anyway
-
+  delay(1);
 }
